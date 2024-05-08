43d3132fabacdb975d5f9576f99d88d93844d208
arturohm41234 <arturoh2672@gmail.com>
2024-05-08 15:21:19 -0600

    create visual component to show the results

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,22 @@
     <h1>Series Numericas</h1>
     <input v-model.number="input" @input="handleInput" type="text" min="0" />
     <p>Serie({{input}}) = {{ sr }}</p>
-    <p>Triangular: {{ tr }}</p>
-    <p>fibonacci: {{ fib }}</p>
-    <p>¿Es primo?: {{ pr }}</p>
-    <p style="font-style: italic; color: gray;">0 no es<br> 1 si es</p>
+    <visual :number="input"></visual>
   </main>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import { triangular } from './series/triangular.ts';
-import { fibonacci } from './series/fibonacci.ts';
-import { primo } from './series/primos.ts';
 import { SeriesNumericas } from './series/serie.ts';
+import visual  from './components/visual.vue';
 
 const input = ref();
-let tr: number | null;
-let fib: number | null;
-let pr: number | null;
 let sr: number | null;
 
 function handleInput() {
   if (input.value >= 0 && Number.isInteger(input.value)) {
-    tr = triangular(input.value);
-    fib = fibonacci(input.value);
-    pr = primo(input.value);
     sr = SeriesNumericas.serie(input.value);
   } else {
-    tr = null;
-    fib = null;
-    pr = null;
     sr = null;
   }
 }
---
+++ src/components/visual.vue
@@ -0,0 +1,25 @@
+<template>
+  <p>Triangular: {{ tr }}</p>
+  <p>fibonacci: {{ fib }}</p>
+  <p>¿Es primo?: {{ pr }}</p>
+  <p style="font-style: italic; color: gray;">0 no es<br> 1 si es</p>
+</template>
+
+<script setup lang="ts">
+import { defineProps, computed }from 'vue';
+import { triangular } from '../series/triangular.ts';
+import { fibonacci } from '../series/fibonacci.ts';
+import { primo } from '../series/primos.ts';
+
+const props = defineProps({
+  number: {
+    type: Number,
+    default: 0,
+    required: true
+  }
+});
+
+const tr = computed(() => { return triangular(props.number); });
+const fib = computed(() => { return fibonacci(props.number); })
+const pr = computed(() => { return primo(props.number); });
+</script>

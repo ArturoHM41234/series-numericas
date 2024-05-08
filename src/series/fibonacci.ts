3ee863a275310f69e16206b5544a2af19a1f9d0c
arturohm41234 <arturoh2672@gmail.com>
2024-05-08 13:19:06 -0600

    Replace fibonacci implementation for a faster one
    The previous implementation was slow on numbers after 30

--- src/series/fibonacci.ts
+++ src/series/fibonacci.ts
@@ -10,11 +10,39 @@
    .
  */
 
-export function fibonacci(n): number {
-  if (n < 2) {
-    return n;
-  } else {
-    return fibonacci(n - 1) + fibonacci(n - 2);
+export function fibonacci(n: number): number {
+  if (n <= 0) {
+    return 0;
+  }
+  
+  let F: number[][] = [[1, 1], [1, 0]]; //Matriz de fibonacci
+
+  power(F, n - 1);
+
+  return F[0][0];
+}
+
+function multiply(F: number[][], M: number[][]) {
+  let x = F[0][0] * M[0][0] + F[0][1] * M[1][0];
+  let y = F[0][0] * M[0][1] + F[0][1] * M[1][1];
+  let z = F[1][0] * M[0][0] + F[1][1] * M[1][0];
+  let w = F[1][0] * M[0][1] + F[1][1] * M[1][1];
+
+  F[0][0] = x;
+  F[0][1] = y;
+  F[1][0] = z;
+  F[1][1] = w;
+}
+
+function power(F: number[][], n: number) {
+  if (n === 0 || n === 1) {
+    return;
+  }
+  let M: number[][] = [[1, 1], [1, 0]];
+  power(F, Math.floor(n / 2));
+  multiply(F, F);
+  if (n % 2 !== 0) {
+    multiply(F, M);
   }
 }
 

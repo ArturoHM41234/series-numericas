82d00ba9e35a235c706d8ff86cfce4d7f79044de
arturohm41234 <arturoh2672@gmail.com>
2024-05-08 18:56:24 -0600

    delete file

--- src/series/serie.ts
+++
@@ -1,9 +0,0 @@
-import { fibonacci } from './fibonacci.ts'
-import { triangular } from './triangular.ts'
-import { primo } from './primos.ts'
-
-export class SeriesNumericas {
-  static serie(n :number): number {
-    return (-3 * primo(n)) - fibonacci(n) + (5 * triangular(n))
-  }
-}

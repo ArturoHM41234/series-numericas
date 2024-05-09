e61daf33a653335939b47dbaff56190746957319
arturohm41234 <arturoh2672@gmail.com>
2024-05-08 18:52:59 -0600

    add case if input 0

--- src/series/triangular.ts
+++ src/series/triangular.ts
@@ -1,6 +1,9 @@
 //Triangular recibe un numero y devuelve el numero triangular de este
 function triangular(n: number): number {
   let tr: number;
+  if (n <= 0) {
+    return 0;
+  }
   tr = (Math.pow(n, 2) + n) / 2;
   return tr;
 }

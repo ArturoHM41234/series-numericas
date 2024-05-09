78c8da2f5a8f2e3a841af52cd1d491fab7162ab6
arturohm41234 <arturoh2672@gmail.com>
2024-05-08 20:51:27 -0600

    Minor fixes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <main>
-    <h1>Series Numericas</h1>
-    <input v-model.number="input" @input="handleInput" type="text" min="0" />
+    <div class="container">
+      <h1>Series Numericas</h1>
+      <label for="numero">Ingresa un numero:</label>
+    <input v-model.number="input" @input="handleInput" type="text" min="0" name="numero" id="numero" />
     <p>Serie({{input}}) = {{ sr }}</p>
-    <visual :number="input || 0"></visual>
+    <visual :number="input || 0"></visual>  
+    </div>
   </main>
 </template>
 
@@ -14,10 +17,11 @@ import visual from './components/visual.vue';
 
 const input = ref(0);
 let sr: number | null;
+const seriesInstance = new SeriesNumericas();
 
 function handleInput() {
   if (input.value >= 0 && Number.isInteger(input.value)) {
-    sr = SeriesNumericas.serie(input.value);
+    sr = seriesInstance.serie(input.value);
   } else {
     sr = null;
   }
--- src/series/series.ts
+++ src/series/series.ts
@@ -3,7 +3,7 @@ import { triangular } from './triangular.ts'
 import { primo } from './primos.ts'
 
 export class SeriesNumericas {
-  static serie(n :number): number {
+  serie(n :number): number {
     return (-3 * primo(n)) - fibonacci(n) + (5 * triangular(n))
   }
 }
